445ca34e6cbdbb88ad469dcc68f3bbba67a55a35
joocod <20634hdj@gmail.com>
2023-12-11 22:06:40 +0900

    cm

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
 
 function Nav() {
     return (
         <div>
             <HeaderContainer>
-                <h1>shop</h1>
+                <h1><Link to='/'>shop</Link></h1>
+                <div className='userWrap'>
+                    <button className='loginBtn'>login</button>
+                </div>
             </HeaderContainer>
         </div>
     )
@@ -12,3 +17,6 @@ function Nav() {
 
 export default Nav
 
+const HeaderContainer = styled.header`
+    
+`
