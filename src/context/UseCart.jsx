e1b4adb7c274b8772af713049c99e2878479ff95
joocod <20634hdj@gmail.com>
2023-12-15 16:30:42 +0900

    commit

---
+++ src/context/UseCart.jsx
@@ -0,0 +1,7 @@
+import { useAuthContext } from "./AuthContext";
+
+export default function useCart(){
+    const {uid} = useAuthContext();
+    // useQueryClient : 리액트에서 데이터를 가져오고 업데이트 하는 라이브러리
+    // 설치 - yarn add @tanstack/react-query
+}   
