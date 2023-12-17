e64bd782112feca630b438eb876df948f0143b07
joocod <20634hdj@gmail.com>
2023-12-18 00:07:28 +0900

    commit

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import UseCart from '../context/UseCart';
 import CartItem from '../components/CartItem';
+import styled from 'styled-components';
 
 function MyCart() {
     const {cartInfo : {data : products}} = UseCart();
@@ -8,17 +9,36 @@ function MyCart() {
 
     return (
         <div className='container'>
-            <h2>장바구니 리스트</h2>
+            <h2 className='itemTitle'>장바구니 리스트</h2>
             {!isItem && <p>장바구니에 상품이 없습니다.</p>}
             {isItem && (
-                <ul className='cartList'>
+                <CartList>
                     {products && products.map((el,index)=>(
                         <CartItem key={el.id} product={el} index={index}/>
                     ))}
-                </ul>
+                </CartList>
             )}
         </div>
     )
 }
 
 export default MyCart
+
+const CartList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    border-top: solid 1px rgba(0,0,0,0.2);
+    padding: 24px 0px;
+    li{
+        display: flex;
+        align-items: center;
+        border-bottom: solid 1px rgba(0,0,0,0.2);
+        padding: 12px 0px;
+        gap: 12px;
+        img{
+            width: 100px;
+            display: block;
+        }
+    }
+`
--- src/style/GlobalStyles.jsx
+++ src/style/GlobalStyles.jsx
@@ -66,5 +66,10 @@ button{
 	margin: 0px auto;
 	padding: 100px 0px;
 }
+.itemTitle{
+	font-size: 24px;
+	margin-bottom: 36px;
+	font-weight: normal;
+}
 `
 export default GlobalStyle;
