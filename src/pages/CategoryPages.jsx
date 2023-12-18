d5eafd5f25a4afe27977e0579a036191bb6d468d
joocod <20634hdj@gmail.com>
2023-12-18 12:34:47 +0900

    commit

--- src/pages/CategoryPages.jsx
+++ src/pages/CategoryPages.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import CategoryProductList from '../components/CategoryProductList';
 
 function CategoryPages() {
+    const {category} = useParams();
+    // console.log(category)
+
     return (
         <div>
-            categoryPages
+            {category}
+            <CategoryProductList category={category}/>
         </div>
     )
 }
