61a1b8dc9cf5c883a72ff57d45ade64ad265421f
joocod <20634hdj@gmail.com>
2023-12-19 17:17:51 +0900

    commit

--- src/pages/Qna.jsx
+++ src/pages/Qna.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Qna() {
+
+    const navigate = useNavigate();
+    const onWriteEvent = ()=>{
+        navigate(`/board/write`,{state : {email : user.email}})
+    }
+
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='board-top'>
+                <h2>QnA 게시판</h2>
+                <button className='writeBtn' onClick={onWriteEvent}>작성하기</button>
+            </div>
         </div>
     )
 }
